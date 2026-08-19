2c7b7d207d0059a2fc48887dc773508f7b199cd5
LAB12_USER ON WS04 <L12X19W04@LabHCI.net>
2026-08-19 20:33:25 +0800

    Create gradient_container.dart

---
+++ lab_act_2/lib/gradient_container.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context){
+    return Container(
+       decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+        Colors.pink,
+        Colors.green
+          ]
+        )
+      ),
+      child: Center(child: Text('Hello World')),
+        );
+  }
+}
--- lab_act_2/lib/main.dart
+++ lab_act_2/lib/main.dart
@@ -1,29 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'gradient_container.dart';
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-    body: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-        Colors.pink,
-        Colors.green
-          ]
-        )
-      ),
-      child: Center(
-        child: Text(
-        'Hello World'),
-        ),
-        ),
-        ),
-      )
-       
-        );
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
 }
+
+
+        
+      
+    
+  
+
 
 
 
