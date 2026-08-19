e1a3926f10374b60f9b5c244f38cde3468d24e2d
LAB12_USER ON WS04 <L12X19W04@LabHCI.net>
2026-08-19 20:15:33 +0800

    Gradient background

--- lab_act_2/lib/main.dart
+++ lab_act_2/lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center( child: Text('Hello World'))));
+  runApp(MaterialApp(
+    home: Scaffold(
+    body: Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+        Colors.pink,
+        Colors.green
+          ]
+        )
+      ),
+      child: Center(
+        child: Text(
+        'Hello World'),
+        ),
+        ),
+        ),
+      )
+       
+        );
 }
+
+
 
