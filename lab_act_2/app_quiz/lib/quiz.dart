a65b2ab6e6ea090218f6b66147d3aad170680319
jsmn761 <jasmineruiz1923@gmail.com>
2026-09-16 14:35:35 +0800

    add files

---
+++ lab_act_2/app_quiz/lib/quiz.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+ 
+import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/question_screen.dart';
+ 
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+ 
+  @override
+  State<Quiz> createState() {
+    return _quizState();
+  }
+}
+ 
+class _quizState extends State<Quiz> {
+  var currentScreen = 'start';
+ 
+  void switchScreen() {
+    setState(() {
+      currentScreen = 'questions';
+    });
+  }
+ 
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+ 
+    if (currentScreen == 'questions') {
+      screenWidget = const QuestionsScreen();
+    }
+ 
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFFF5F1E8),
+                Color(0xFFE8EFE5),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
